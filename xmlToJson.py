a05ab4250543e69f2489b1cbff0124af8844a38c
CristhianMotoche <cristhian.motoche@gmail.com>
2018-01-07 19:38:16 -0500

    Add script to transform from XML to JSON

--- xmlToJson.py
+++ xmlToJson.py
@@ -1,16 +1,18 @@
 import json
 import xmltodict
 
-with open('sample.json', 'r') as f:
-    jsonString = f.read()
+with open('sample.xml', 'r') as fileXML:
+    xmlString = fileXML.read()
+fileXML.close()
 
-print('JSON input (sample.json):')
-print(jsonString)
-
-xmlString = xmltodict.unparse(json.loads(jsonString), pretty=True)
-
-print('\nXML output(output.xml):')
+print('XML input (sample.xml):')
 print(xmlString)
 
-with open('output.xml', 'w') as f:
-    f.write(xmlString)
+jsonString = json.dumps(xmltodict.parse(xmlString))
+
+print('\nJSON output(output.json):')
+print(jsonString)
+
+with open('output.json', 'w') as fileJSON:
+    fileJSON.write(jsonString)
+fileJSON.close()
